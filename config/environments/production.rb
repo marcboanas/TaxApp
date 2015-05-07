0eea97984d884121bbfed0884817344968fa216e
Marc Boanas <marcjohnboanas@gmail.com>
2015-05-07 20:25:40 +0100

    sendgrid settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   host = "protected-island-2522.herokuapp.com"
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address => 'smpt.sendgrid.net',
+    :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
